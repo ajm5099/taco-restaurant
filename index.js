b9462da5e4e5d2647a498d5d0adbffa8105b787e
“karzoun” <karzoun_27@hotmail.com>
2020-10-07 13:06:25 -0700

    overview

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Data
-const tables = [
-    {
+const tables = [{
         Name: 'john',
         party: '4',
         Seating: 'indoor'
@@ -37,40 +36,49 @@ const tables = [
         Seating: 'indoor'
     }
 ];
-const reservations = [
-    {
-        Name: 'billy',
-        party: '2',
-        Seating: 'outdoor'
-    }
-];
+const reservations = [{
+    Name: 'billy',
+    party: '2',
+    Seating: 'outdoor'
+}];
 
 // Routes
 // ===========================================================
-app.get("/", function (req, res) {
-    res.send("Welcome to the Taco Restaurant");
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+
 });
 
-app.get('/api/tables', function (req, res) {
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+
+});
+app.get("/reservations", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservations.html"));
+
+});
+
+app.get('/api/tables', function(req, res) {
     return res.json(tables);
 })
 
-app.get('/api/reservations', function (req, res) {
+app.get('/api/reservations', function(req, res) {
     return res.json(reservations);
 })
 
-if (tables.length <= 5) {
-    app.post('/api/tables', function (req, res) {
+
+app.post('/api/tables', function(req, res) {
+    if (tables.length <= 5) {
         tables.push(req.body);
-        res.json(tables);
-        return res.json(false);
-    })
-} else {
-    app.post('/api/reservations', function (req, res) {
+        res.send('you have a Table!');
+
+    } else {
         reservations.push(req.body);
-        res.json(reservations);
-    })
-}
+        res.send('you have been added to the waiting list!');
+
+    }
+})
+
 
 
 
@@ -79,7 +87,7 @@ if (tables.length <= 5) {
 
 // Listener
 // ===========================================================
-app.post("/api/tables", function (req, res) {
+app.post("/api/tables", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newTable = req.body;
@@ -93,7 +101,7 @@ app.post("/api/tables", function (req, res) {
     res.json(newTable);
 });
 
-app.post("/api/reservations", function (req, res) {
+app.post("/api/reservations", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newReservation = req.body;
@@ -108,6 +116,6 @@ app.post("/api/reservations", function (req, res) {
 });
 
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
